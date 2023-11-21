c1d676bd931529d2acc509e7bf4b433f4b4d0e6d
Tamimul Islam <hmtamim24@gmail.com>
2023-11-21 18:01:03 +0600

    astrojs/preact package added for interactiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-blog-24.netlify.app/"
+  site: "https://astro-blog-24.netlify.app/",
+  integrations: [preact()]
 });
